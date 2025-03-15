89b1716c0b95ceb50bd0f601818c2ee204dd55b4
Matheus H. Potrich <matheushp.com@hotmail.com>
2025-03-15 00:26:24 -0300

    fixed not reading the default file

--- builder.cpp
+++ builder.cpp
@@ -3,14 +3,21 @@
 #include <fstream>
 #include <iostream>
 #include <filesystem>
+#include <vector>
 
-void read_file_content(Json::Value file_content_row)
+std::vector<File_Content *> read_file_content(Json::Value file_content_row)
 {
+	std::vector<File_Content *> all_file_content;
+	
 	for (const auto& item : file_content_row)
 	{
 		File_Content* file_content = new File_Content(item["id"].asString(), item["path"].asString());
+
+		all_file_content.push_back(file_content);
 		std::cout << "file content loaded successful: " << file_content->loaded_successful << std::endl;
-	}	
+	}
+
+	return all_file_content;
 }
 
 void create_folder(std::string path)
@@ -19,38 +26,48 @@ void create_folder(std::string path)
 	std::filesystem::create_directories(path);
 }
 
-void create_file(std::string path, std::string content)
+std::string check_file_content_id(std::string id, std::vector<File_Content *> all_file_content)
+{
+	for (File_Content* file_content : all_file_content)
+	{
+		if(file_content->check_id(id)) return file_content->content;	
+	}
+
+	return id;
+}
+
+void create_file(std::string path, std::string content_id, std::vector<File_Content *> all_file_content)
 {
 	std::cout << "creating file in: " << path << std::endl;
 	std::ofstream outfile(path);
-	outfile << content;
+	outfile << check_file_content_id(content_id, all_file_content);
 	outfile.close();
 }
 
-void create_folder_content(std::string path, Json::Value current_object, Json::Value current_object_key)
+void create_folder_content(std::string path, Json::Value current_object, Json::Value current_object_key, std::vector<File_Content *> all_file_content)
 {
 	std::string temp_path_file = path + "/" + current_object_key.asString();
 
 	// file
 	if (current_object.isString())
 	{
-		create_file(temp_path_file, current_object.asString());
+		create_file(temp_path_file, current_object.asString(), all_file_content);
 	}
 	// folder
 	else if (current_object.isObject())
 	{
 		create_folder(temp_path_file);
-		build_structure(temp_path_file, current_object);
+		build_structure(temp_path_file, current_object, all_file_content);
 	}
 }
 
-void build_structure(std::string structure_path, Json::Value parent_object)
+void build_structure(std::string structure_path, Json::Value parent_object, std::vector<File_Content *> all_file_content)
 {
 	std::string new_path = structure_path;
 
 	for(const auto& object_child : parent_object.getMemberNames())
 	{
-		create_folder_content(new_path, parent_object[object_child], object_child);
+		create_folder_content(new_path, parent_object[object_child], object_child, all_file_content);
 	}
 
 	//free(new_path);
@@ -59,6 +76,8 @@ void build_structure(std::string structure_path, Json::Value parent_object)
 void build(Json::Value template_content)
 {
 	std::string root_folder = std::string(".");
-	read_file_content(template_content["file-content"]);
-	build_structure(root_folder, template_content["structure"]);
+	std::vector<File_Content *> all_file_content;
+
+	all_file_content = read_file_content(template_content["file-content"]);
+	build_structure(root_folder, template_content["structure"], all_file_content);
 }
--- builder.h
+++ builder.h
@@ -1,5 +1,7 @@
 #include <jsoncpp/json/json.h>
 #include <fstream>
+#include <vector>
+#include <iostream>
 
 #ifndef BUILDER_H
 #define BUILDER_H
@@ -19,12 +21,16 @@ private:
 	{
 		if(this->path.empty()) return false;
 
+		std::cout << "loding content from: " << this->path << std::endl;
+
 		std::string content;
 		std::ifstream content_file(this->path);
 
 		while (getline (content_file, content))
 		{
-			this->content = this->content + content;
+			std::string string_content(content);
+			std::cout << "content loading from " << this->path << " " << string_content << std::endl;
+			this->content += string_content;
 		}
 
 		this->update_dynamic_tags();
@@ -43,14 +49,20 @@ public:
 		this->id = id;
 		this->path = file_path;
 		this->loaded_successful = load_content();
+	}
+
+	bool check_id(std::string id)
+	{
+		return this->id == id;
 	}	
 };
 	
-void read_file_content(Json::Value file_content_row);
+std::vector<File_Content *> read_file_content(Json::Value file_content_row);
 void create_folder(std::string path);
-void create_file(std::string path, std::string content);
-void create_folder_content(std::string path, Json::Value current_object, Json::Value current_object_key);
-void build_structure(std::string structure_path, Json::Value parent_object);
+std::string check_file_content_id(std::string id, std::vector<File_Content *> all_file_content);
+void create_file(std::string path, std::string content, std::vector<File_Content *> all_file_content);
+void create_folder_content(std::string path, Json::Value current_object, Json::Value current_object_key, std::vector<File_Content *> all_file_content);
+void build_structure(std::string structure_path, Json::Value parent_object, std::vector<File_Content *> all_file_content);
 void build(Json::Value template_content);
 
 #endif /* BUILDER_H */
